28a578f4fe33385ae268de4892de6358bbfdf2a8
jnears <jnears@essex.ac.uk>
2014-04-28 17:00:44 +0100

    Improve 404 page and
    Changed wording, removed smilie and added the right sidebar nag  to the
    page.

--- 404.php
+++ 404.php
@@ -7,16 +7,16 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'societycentral' ); ?></h1>
+					<h1 class="page-title"><?php _e( 'The page your looking for can&rsquo;t be found.', 'societycentral' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'societycentral' ); ?></p>
+					<p><?php _e( 'Please check that you have entered the correct web address, or try one of the links below or a search', 'societycentral' ); ?></p>
 
 					<?php get_search_form(); ?>
 
@@ -41,7 +41,7 @@ get_header(); ?>
 
 					<?php
 					/* translators: %1$s: smiley */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'societycentral' ), convert_smilies( ':)' ) ) . '</p>';
+					$archive_content = '<p> Try looking in the monthly archives.</p>';
 					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 					?>
 
@@ -53,4 +53,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
