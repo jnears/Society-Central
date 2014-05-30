e32635e3779dbb061f708da4ce18b715e7829f19
jnears <jnears@essex.ac.uk>
2014-05-30 14:23:13 +0100

    Add font awesome

--- functions.php
+++ functions.php
@@ -90,11 +90,14 @@ function societycentral_scripts() {
 
 	wp_enqueue_script( 'societycentral-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+	wp_enqueue_style( 'prefix-font-awesome', get_template_directory_uri() . '/font-awesome/css/font-awesome.min.css', array(), '4.1.0' );
+
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'societycentral_scripts' );
+
 
 /**
  * Implement the Custom Header feature.
