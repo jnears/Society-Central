c261f650f7290d49a7260f2173c11115def6a48c
jnears <jnears@essex.ac.uk>
2014-05-30 11:37:11 +0100

    Format code in sidebar

--- sidebar.php
+++ sidebar.php
@@ -6,31 +6,28 @@
  */
 ?>
 <div id="secondary" class="widget-area" role="complementary">
+	
 	<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-
 	<?php endif; // end sidebar widget area ?>
 
-
 	<?php if ( is_home() ) : // show events listing on homepage?>
-	<aside class="widget">
-<p class="lead">
-	  <?php  the_author_meta('excerpt',8); ?></p>
-	</aside>
+		<aside class="widget">
+			<p class="lead">
+	  	<?php  the_author_meta('excerpt',8); ?></p>
+		</aside>
 	<?php endif;  ?>
 
 	<!-- Dont show navigation on single page -->
 	<?php if ( ! is_single() ){?>
-	 <aside id="headlines" class="widget">
+	 	<aside id="headlines" class="widget">
 			<?php echo headline_list(10); ?>
 		</aside>
 	<?php }?>
 
-
 	<?php if ( is_home() ) : // show events listing on homepage?>
-
-<aside id="events-list" class="widget">
-<?php the_widget('TribeEventsListWidget'); ?>
-</aside>
+		<aside id="events-list" class="widget">
+			<?php the_widget('TribeEventsListWidget'); ?>
+		</aside>
 	<?php endif;  ?>
 
 </div><!-- #secondary -->
