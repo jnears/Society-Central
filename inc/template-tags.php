4e561cf198e526b374d35df1560207441bec7afe
jnears <jnears@essex.ac.uk>
2014-06-13 14:39:03 +0100

    Remove margin for mobile menu

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -70,16 +70,6 @@ function societycentral_posted_on() {
 	if ( get_the_author_meta( 'jobtitle' ) ) { 
 		$author_jobtitle =  '- ' . esc_html(get_the_author_meta( 'jobtitle' )) ;
 	};
-	// if ( get_the_author_meta( 'user_firstname' ) ) { 
-	// 	$author_firstname =  esc_html(get_the_author_meta( 'user_firstname' )) ;
-	// };
-
-	// if ( get_the_author_meta( 'user_lastname' ) ) { 
-	// 	$author_lastname =  esc_html(get_the_author_meta( 'user_lastname' )) ;
-	// };
-	// if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-	// 	$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
-	// }
 
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
@@ -88,12 +78,8 @@ function societycentral_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	// printf( __( '<span class="byline">%1$s </span><span class="posted-on"> %1$s</span>', 'societycentral' ),
 	printf( __( '<span class="byline">%1$s </span>', 'societycentral' ),
-		// sprintf( '<a href="%1$s" rel="bookmark">%2$s</a>',
-		// 	esc_url( get_permalink() ),
-		// 	$time_string
-		// ),
+		
 		sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s">%2$s %3$s</a> %4$s</span> %5$s',
 			
 			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
@@ -105,8 +91,6 @@ function societycentral_posted_on() {
 	);
 }
 endif;
-
-
 
 
 
