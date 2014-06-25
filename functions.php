74e1c4bb94d6a02f5c92cdf58be49487d69770ce
jnears <jnears@essex.ac.uk>
2014-06-25 17:14:48 +0100

    Remove query from functions

--- functions.php
+++ functions.php
@@ -422,13 +422,6 @@ function delete_retina_support_images( $attachment_id ) {
 }
 
 
-if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
-function my_jquery_enqueue() {
-   wp_deregister_script('jquery');
-   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js", false, null);
-   wp_enqueue_script('jquery');
-}
-
 
 // //baseline.js - set image margins to
 // function add_baseline() {
