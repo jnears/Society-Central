2e7a6521a255d80ec1e5791e1057f157dae1291d
jnears <jnears@essex.ac.uk>
2014-07-10 10:07:50 +0100

    Add favicon

--- header.php
+++ header.php
@@ -22,12 +22,13 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js" type="text/javascript"></script>
 <![endif]-->
-
-<?php wp_head(); ?>
-<!--[if IE 7]>
+<!--[if IE 8]>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/font-awesome/css/font-awesome-ie7.min.css">
 <![endif]-->
+<?php wp_head(); ?>
 <link rel="stylesheet" type="text/css" media="print" href="<?php bloginfo('stylesheet_directory'); ?>/print.css" />
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 </head>
 
 <body <?php body_class(); ?>>
